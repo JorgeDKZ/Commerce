ae872662fdfba5dcacebcd136e71420bbd42132b
Jorge Navarro <130685403+JorgeNavarro-Programmer@users.noreply.github.com>
2025-11-06 12:09:50 +0100

    Password encoder anded and feing client to connet with clientserver

--- microservice-auth/src/main/java/com/jorgedkz/microservice_auth/MicroserviceAuthApplication.java
+++ microservice-auth/src/main/java/com/jorgedkz/microservice_auth/MicroserviceAuthApplication.java
@@ -2,9 +2,11 @@ package com.jorgedkz.microservice_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableFeignClients
 @EnableWebSecurity
 public class MicroserviceAuthApplication {
 
